185ddcb53958f2b8775746526ae17ba1ab94316c
Cris-Z <48642291+crizan@users.noreply.github.com>
2021-01-31 23:45:34 +0100

    fix colors but moved from shader to loading

--- main.cpp
+++ main.cpp
@@ -53,7 +53,7 @@ void create_textures(std::unique_ptr<rlottie::Animation>& animation, std::vector
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer.data());
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_BYTE, buffer.data());
     }
 }
 
@@ -101,7 +101,7 @@ const char* fragment_shader =
 "uniform sampler2D Texture;"
 "void main()"
 "{"
-"   FragColor = vec4(texture(Texture, TexCoord).bgr, 1.0);"
+"   FragColor = texture(Texture, TexCoord);"
 "}"
 ;
 
